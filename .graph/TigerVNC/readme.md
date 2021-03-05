ca8632f989754bd2be102aa722634bd207c35e93
Pegasus <pegasus@unisocial.net>
2021-03-05 09:47:27 -0500

    broken

--- .graph/TigerVNC/readme.md
+++ .graph/TigerVNC/readme.md
@@ -5,3 +5,6 @@ https://wiki.archlinux.org/index.php/TigerVNC
 ## Ubuntu 20.04
 - https://www.tecmint.com/install-and-configure-vnc-server-on-ubuntu/
 - https://www.answertopia.com/ubuntu/ubuntu-remote-desktop-access-with-vnc/
+
+## Ubuntu 18.04
+- https://www.cyberciti.biz/faq/install-and-configure-tigervnc-server-on-ubuntu-18-04/
--- roles/tigervnc/files/start.sh
+++ roles/tigervnc/files/start.sh
@@ -1,11 +1,11 @@
 # Works as Sudo but not as standard user! I Still don't know why?
 
 default(){
-sudo vncserver :1 -localhost no -geometry 1024x768
+  sudo vncserver :1 -localhost no -geometry 1024x768
 }
 
 depth(){
-sudo vncserver :1 -localhost no -geometry 1024x768 -depth 32
+  sudo vncserver :1 -localhost no -geometry 1024x768 -depth 32
 }
 
 default
